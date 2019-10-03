9713c2cb99b01fe484abe013b1ff2198382c4787
William Sedlacek <wsedlacekc@gmail.com>
2019-10-03 13:11:58 -0500

    Implement test
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/calculator.spec.ts
+++ src/calculator.spec.ts
@@ -1,6 +1,5 @@
-import { add } from './calculator';
+import { add, subtract } from './calculator';
 
-// test away!
-test('5 + 10 = 15', () => {
-  expect(add(5, 10)).toBe(15);
-});
+test('5 + 10 = 15', () => expect(add(5, 10)).toBe(15));
+test('5 - 10 = -5', () => expect(subtract(5, 10)).toBe(-5));
+test('NaN - 5 = NaN', () => expect(subtract(NaN, 5)).toBe(NaN));
